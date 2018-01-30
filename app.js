0daa4c23c672e3de446512bbf290c7d1564cdf6c
Keli Hansen <keli.p.hansen@gmail.com>
2018-01-30 15:02:00 -0800

    added first guessing game question

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-‘use strict’;
+'use strict';
 
 const userName = prompt('Hello! What\'s your name?');
 alert('Hi, ' + userName + '! I hope you don\'t mind if I ask you a few more questions.');
 console.log('The user\'s name is ' + userName + '.');
 
-const userHome = prompt('Where are you from?');
+/*const userHome = prompt('Where are you from?');
 alert('How nice, ' + userName + '. I hear ' + userHome + ' is beautiful this time of year.');
 console.log(userName + ' is from ' + userHome + '.');
 
@@ -14,7 +14,16 @@ console.log(userName + '\'s favorite food is ' + favoriteFood + '.');
 
 const favoriteSong = prompt('What\s your favorite song?');
 alert('I can picture you now, ' + userName + ' from ' + userHome + '. Dancing to ' + favoriteSong + ' while eating ' + favoriteFood + '.');
-console.log(userName + '\'s favorite song is ' + favoriteSong + '.');
+console.log(userName + '\'s favorite song is ' + favoriteSong + '.');*/
+
+const hateCats = prompt('Enough about you, ' + userName + '. I\'m guessing you\'d like to know everything about me! First things first. Do you think I hate cats?').toLowerCase();
+if (hateCats === 'yes' || hateCats === 'y') {
+    alert('Silly ' + userName + '. No one could hate cats.');
+} else if (hateCats === 'no' || hateCats === 'n') {
+    alert('You know me so well already. Of course I don\'t!');
+} else {
+    alert('You can answer Yes or No (or yes or no or YeS or nO or even y or n, but you can\'t answer ' + hateCats + '! Let\'s try another one.');
+}
 
 /* Enough about you, userName. I'm guessing you'd like to know everything about me! 
 First things first. Do you think I hate cats?
@@ -30,7 +39,7 @@ Y: I do. I really do.
 N: But userName, the crashing waves! The craggy coves! The dramatic light!
 What do you think: Can I sit still when I hear Gloria by Laura Branigan?
 Y: Oh userName. I thought you knew me.
-N: It's true, I can't. Not even a little.
+N: It's true, I can't. Not even close.
 Okay userName. Last question. Are sweaters my favorite thing to knit?
 Y: Good guess, but I actually prefer hats.
 N: Clever userName. I've never even finished a sweater!
