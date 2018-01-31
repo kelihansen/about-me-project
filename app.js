8191893f3c13c4713b17b32c6b532d66793e4621
Keli Hansen <keli.p.hansen@gmail.com>
2018-01-30 16:27:26 -0800

    copy/pasted content in for 3 additional guessing game questions

--- app.js
+++ app.js
@@ -50,6 +50,48 @@ if (rideBike === 'yes' || rideBike === 'y') {
 console.log('When asked if I ride my bike to school, ' + userName + ' answered "' + rideBike + '."');
 console.log(userName + ' is ' + userStatus + '.');
 
+const lovesPacific = prompt('All right, ' + userName + '. Do I think cold, rocky, Pacific beaches are some of the prettiest places on earth?').toLowerCase();
+if (lovesPacific === 'yes' || lovesPacific === 'y') {
+    alert('I do. I really do.');
+    userStatus = 'correct';
+} else if (lovesPacific === 'no' || lovesPacific === 'n') {
+    alert('But ' + userName + ', the crashing waves! The craggy coves! The dramatic light!');
+    userStatus = 'incorrect';
+} else {
+    alert('You can answer Yes or No (or yes or no or YeS or nO or even y or n, but you can\'t answer ' + lovesPacific + '! Let\'s try another one.');
+    userStatus = 'not playing by the rules';
+}
+console.log('When asked if I think cold, rocky, Pacific beaches are some of the prettiest places on earth, ' + userName + ' answered "' + lovesPacific + '."');
+console.log(userName + ' is ' + userStatus + '.');
+
+const unmovedByGloria = prompt('What do you think: Can I sit still when I hear Gloria by Laura Branigan?').toLowerCase();
+if (unmovedByGloria === 'yes' || unmovedByGloria === 'y') {
+    alert('Oh ' + userName + '. I thought you understood me.');
+    userStatus = 'incorrect';
+} else if (unmovedByGloria === 'no' || unmovedByGloria === 'n') {
+    alert('It\'s true, I can\'t. Not even close.');
+    userStatus = 'correct';
+} else {
+    alert('You can answer Yes or No (or yes or no or YeS or nO or even y or n, but you can\'t answer ' + unmovedByGloria + '! Let\'s try another one.');
+    userStatus = 'not playing by the rules';
+}
+console.log('When asked if I can sit still when I hear Gloria by Laura Branigan, ' + userName + ' answered "' + unmovedByGloria + '."');
+console.log(userName + ' is ' + userStatus + '.');
+
+const loveSweaters = prompt('Okay ' + userName + '. Last question. Are sweaters my favorite thing to knit?').toLowerCase();
+if (loveSweaters === 'yes' || loveSweaters === 'y') {
+    alert('Good guess, but I actually prefer hats.');
+    userStatus = 'incorrect';
+} else if (loveSweaters === 'no' || loveSweaters === 'n') {
+    alert('Clever ' + userName + '. I\'ve never even finished a sweater!');
+    userStatus = 'correct';
+} else {
+    alert('You can answer Yes or No (or yes or no or YeS or nO or even y or n, but you can\'t answer ' + loveSweaters + '! Let\'s try another one.');
+    userStatus = 'not playing by the rules';
+}
+console.log('When asked if sweaters are my favorite thing to knit, ' + userName + ' answered "' + loveSweaters + '."');
+console.log(userName + ' is ' + userStatus + '.');
+
 /* Enough about you, userName. I'm guessing you'd like to know everything about me!
 First things first. Do you think I hate cats?
 Y: Silly userName. No one could hate cats.
